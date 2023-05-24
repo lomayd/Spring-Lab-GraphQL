c1198ad160a8e59d6a673740cb0b55527dedd86c
lomayd <jinho10167@gmail.com>
2023-05-24 01:19:01 +0000

    feat: Project Initialize

---
+++ src/main/java/lomayd/SpringLabGraphQL/api/SpringLabGraphQlApplication.java
@@ -0,0 +1,13 @@
+package lomayd.SpringLabGraphQL.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLabGraphQlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lomayd/SpringLabGraphQL/SpringLabGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package lomayd.SpringLabGraphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLabGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
